2eda4e2a69c97fc43289fb3392927ac238b80b7e
jasonchen <944739385@qq.com>
2020-11-21 22:29:24 +0800

    20201121 第二版

---
+++ HuaShiMicroApp/Bean/Result.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HuaShiMicroApp.Bean.Enum;
+
+namespace HuaShiMicroApp
+{
+    public class Result: object
+    {
+        //index
+        public int index;
+
+        // startTime
+        public DateTime startTime;
+        // endTime
+        public DateTime? endTime = null;
+        //选择使用时间
+        public int? usedTime = null;
+        //是否选择正确
+        public bool? chooseIfCorrent  = null;
+
+        //类型
+        private String type;
+
+        public void setEndTime()
+        {
+            DateTime now = DateTime.Now;
+            this.endTime = now;
+            TimeSpan? period = this.endTime - this.startTime;
+            usedTime = (int)period.Value.TotalMilliseconds;
+            //usedTime = this.endTime.Millisecond - this.startTime.Millisecond;
+        }
+
+        public void setStartTime()
+        {
+            this.startTime = DateTime.Now;
+        }
+
+        public void setChooseIfCorrect(int randIndex, int chooseIndex)
+        {
+            if (randIndex == chooseIndex)
+            {
+                this.chooseIfCorrent = true;
+            }
+            else
+            {
+                this.chooseIfCorrent = false;
+            }
+        }
+
+        // point 0左 1右 2上 3下
+        public void setType(ShowType showType, DisappearType disappearType, String position) {
+            String num1 = disappearType == DisappearType.DISAPPEAR_ORDER ? "0" : "1";
+            String num2 = (showType == ShowType.PERSON_AND_PERSON || showType == ShowType.PERSON_AND_THING) ?  "1" : "0";
+            String num3 = (showType == ShowType.THING_AND_PERSON || showType == ShowType.PERSON_AND_PERSON) ? "1" : "0";
+            this.type = num1 + num2 + num3 + position;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("index: {0}, useTime: {1}, type: {2}, correct: {3}", this.index, this.usedTime == null? "null": this.usedTime.ToString()
+                , this.type, this.chooseIfCorrent==null?"null":this.chooseIfCorrent.ToString());
+        }
+    }
+}
---
+++ HuaShiMicroApp/Util/LayoutUtil.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HuaShiMicroApp.Util
+{
+    class LayoutUtil
+    {
+        // 得到控件的中间位置
+        public static Point getCenterLocation(Form form, Control control) { 
+         return new Point{
+             X = form.Size.Width/2 - control.Width/2,
+             Y = form.Size.Height / 2 - control.Height / 2 - 50
+         };
+        }
+
+        // 得到控件的左边位置
+       public static Point getLeftLocation(Form form, Control control)
+        {
+            return new Point
+            {
+                X = 0,
+                Y = form.Size.Height / 2 - control.Height / 2 - 50 //Y要上偏移，显示的问题
+            };
+        }
+
+        //得到控件的右边位置
+        public static Point getRightLocation(Form form, Control control)
+        {
+            return new Point
+            {
+                X = form.Size.Width  - control.Width,
+                Y = form.Size.Height / 2 - control.Height / 2 - 50
+            };
+        }
+
+        //得到控件的上边位置
+        public static Point getUpLocation(Form form, Control control)
+        {
+            return new Point
+            {
+                X = form.Size.Width / 2 - control.Width / 2,
+                Y = 0
+            };
+        }
+
+        //得到控件的下边位置
+        public static Point getDownLocation(Form form, Control control)
+        {
+            return new Point
+            {
+                X = form.Size.Width / 2 - control.Width / 2,
+                Y = form.Size.Height - control.Height - 80
+            };
+        }
+
+    }
+    
+}
